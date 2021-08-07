48fe643374a74bbe1c9eeb1e2652db7864471334
RomanoInTheAnalysis <84927570+RomanoInTheAnalysis@users.noreply.github.com>
2021-08-07 17:06:46 +0200

    Update script_cleaning.py

--- script_cleaning.py
+++ script_cleaning.py
@@ -4,4 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 df=pd.read_csv('D:\projet_energie.csv', sep=';', low_memory=False)
-print(df.head())
+df.head()
+
+#Création “Date_datetime” au format date :
+from datetime import datetime as dt
+
+df["Date_datetime"]=pd.to_datetime(df.Date)
+df.head()
+
+#“Heure” au format heure
+from datetime import datetime as dt
+
+df["Heure"]=df.Heure.astype("datetime64")
+df["Heure"]=df.Heure.dt.hour
+df.head()
+
+#Affichage des colonnes du dataset
+print(df.columns)
