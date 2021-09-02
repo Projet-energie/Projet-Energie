d278d1f3c796f7115a8537bd63a13e44315daad4
RomanoInTheAnalysis <84927570+RomanoInTheAnalysis@users.noreply.github.com>
2021-09-02 16:12:33 +0200

    Delete script_nettoyage.py

--- script_nettoyage.py
+++
@@ -1,7 +0,0 @@
-# Importer le fichier et afficher les premières lignes :
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-df=pd.read_csv('D:\projet_energie.csv', sep=';', low_memory=False)
-print(df.head())
